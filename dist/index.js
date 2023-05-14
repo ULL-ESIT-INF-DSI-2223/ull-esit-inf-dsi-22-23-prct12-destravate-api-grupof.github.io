d0f7b6101b06d76cd705679d011a4f101efc2122
Gabi <Alu0101098340@ull.edu.es>
2023-05-14 15:32:13 +0000

    dist

--- dist/index.js
+++ dist/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 const uri = "mongodb+srv://user:user123ASDFG@cluster0.mow4vh4.mongodb.net/?retryWrites=true&w=majority";
 const uriLocal = "mongodb://127.0.0.1:27017/destravate";
-connect(uriLocal).then(() => {
+connect(uri).then(() => {
     console.log('Connection to MongoDB server established');
 }).catch(() => {
     console.log('Unable to connect to MongoDB server');
