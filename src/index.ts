bfaacb29b6ef6a63563fbd4d3f9d132a25c8f7d2
Gabi <Alu0101098340@ull.edu.es>
2023-05-14 16:05:08 +0000

    Mongo

--- src/index.ts
+++ src/index.ts
@@ -11,13 +11,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri ="mongodb+srv://user:user123ASDFG@cluster0.mow4vh4.mongodb.net/?retryWrites=true&w=majority";
-const uriLocal = "mongodb://127.0.0.1:27017/destravate";
 
-connect(uri).then(() => {
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://user:user123ASDFG@cluster0.mow4vh4.mongodb.net/?retryWrites=true&w=majority";
+
+connect(MONGO_URI).then(() => {
   console.log('Connection to MongoDB server established');
 }).catch(() => {
   console.log('Unable to connect to MongoDB server');
+});
+
 });
 
 app.use('/users', usersRouter);
